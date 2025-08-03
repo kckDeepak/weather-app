7ae8f5b266055448d621553a80d6255affd00842
kckDeepak <kckdeepak29@gmail.com>
2025-08-03 14:37:57 +0530

    about page modified

--- frontend/weather-app-ui/src/components/About.js
+++ frontend/weather-app-ui/src/components/About.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import Header from './Header'
+import { Typography } from '@mui/material'
 
 export default function About() {
   return (
-    <div>About</div>
+    <>
+    <Header/>
+    <Typography variant='h6' component="div" sx={{mt:4}} display='flex' justifyContent='center'>
+        Hi, I am Chaitanya 
+        <br/>
+        This is a basic weather app that I built using React and Express
+    </Typography>
+    </>
   )
 }
